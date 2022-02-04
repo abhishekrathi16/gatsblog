84437e5627a31dfdd1ab70300846ded70e84d69a
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-02-04 20:16:05 +0530

    added absolute imports

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,17 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+// exports.createPages = async ({ actions }) => {
+//   const { createPage } = actions
+//   createPage({
+//     path: "/using-dsg",
+//     component: require.resolve("./src/templates/using-dsg.js"),
+//     context: {},
+//     defer: true,
+//   })
+// }
+
+const path = require("path")
+//for absolute imports
+exports.onCreateWebpackConfig=({actions})=>{
+  actions.setWebpackConfig({
+    resolve: {modules: [path.resolve(__dirname,"src"), "node_modules"]},
   })
 }
